516902814cc6dfe24a182245c67e3a469973e033
dtochylin <ditochilin@gmail.com>
2021-05-03 22:24:38 +0300

    Added task sortStrings
    added logic buble sort method

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,29 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const arrSorted = arr.slice();
+  let n = arrSorted.length;
+  
+  while (n > 1) {
+    let newn = 0;
+    let index = 1;
+    while (index <= n - 1) {
+      const compareResult = getLocalCompare(arrSorted, index);
+      if (compareResult > 0 && param === 'asc'
+        || compareResult < 0 && param === 'desc') {
+        // swap
+        const bufferStr = arrSorted[index - 1];
+        arrSorted[index - 1] = arrSorted[index];
+        arrSorted[index] = bufferStr;
+        newn = index;
+      }
+      index++;
+    }
+    n = newn;
+  }
+  return arrSorted;
+}
 
+function getLocalCompare(arrSorted, index) {
+  return arrSorted[index - 1].localeCompare(arrSorted[index],'ru', { caseFirst: 'upper' });
 }
