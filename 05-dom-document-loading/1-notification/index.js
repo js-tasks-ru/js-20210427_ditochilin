18f43d1f1cf50e257a829e3c0fd570018188f01a
dtochylin <ditochilin@gmail.com>
2021-05-21 11:44:19 +0300

    Added task SortableTable компонент - без обработчиков событий клика и динамической загрузки
    05-dom task

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,62 @@
 export default class NotificationMessage {
+    
+    static currentMessage;
+    constructor(message, settings) {
+        this.update(message, settings);
+        this.dialogBox = document.querySelector('#targetDialog');
+    }
 
+    render() {        
+        const element = document.createElement('div');
+        element.innerHTML = this.dialogBoxBuilder(this.message, this.type, this.duration / 1000);        
+        this.element = element.firstElementChild;
+
+        NotificationMessage.currentMessage = this.element;
+    }
+
+    update(message, settings) {
+        if(NotificationMessage.currentMessage) {
+            NotificationMessage.currentMessage.remove();
+        }
+        this.message = message;
+        
+        if (settings) {      
+            this.duration = settings.duration;
+            this.type = settings.type;         
+        }
+        this.render();
+      }
+
+    show(targetDiv) {
+        if(targetDiv) {
+            targetDiv.append(this.element);
+        }
+        
+         setTimeout(() => {            
+             this.destroy();         
+         }, this.duration);
+    }
+
+
+    dialogBoxBuilder(message, type, duration) {
+        return `
+        <div class="notification ${type}" style="--value:${duration}s">
+            <div class="timer"></div>
+            <div class="inner-wrapper">
+                <div class="notification-header">${type}</div>
+                <div class="notification-body">
+                    ${message}
+                </div>
+            </div>
+         </div>
+      `;
+    }
+
+    remove() {
+        this.element.remove();
+    }
+
+    destroy() {
+        this.remove();
+    }
 }
